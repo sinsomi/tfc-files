adea079c711942cf81a0c6ee6e8b49eed96dc78d
sinsomi <49431698+sinsomi@users.noreply.github.com>
2022-07-12 14:26:36 +0900

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,7 +1,7 @@
 variable "region" {
   description = "The region Terraform deploys your instances"
   type        = string
-  default     = "us-east-1"
+  default     = "eu-west-1"
 }
 
 variable "vpc_cidr_block" {

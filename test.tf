0d04d04154090157468441e4c714e9d628b59858
sinsomi <49431698+sinsomi@users.noreply.github.com>
2022-07-12 15:09:05 +0900

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,10 +1,10 @@
-resource "aws_instance" "ireland" {
+resource "aws_instance" "test" {
   count = var.enable_test_env ? var.test_instance_count : 0
 
   ami                    = data.aws_ami.amazon_linux.id
   instance_type          = "t2.micro"
    
-  key_name		 = "ireland"
+  key_name		 = "test"
 
   subnet_id              = module.vpc.public_subnets[count.index % length(module.vpc.public_subnets)]
   vpc_security_group_ids = [module.ssh_security_group.this_security_group_id]
@@ -17,14 +17,14 @@ resource "aws_instance" "ireland" {
   }
 }
 
-resource "aws_lb_target_group" "ireland" {
+resource "aws_lb_target_group" "test" {
   name     = "test-tg-${random_pet.app.id}-lb"
   port     = 22
   protocol = "TCP"
   vpc_id   = module.vpc.vpc_id
 }
 
-resource "aws_lb_target_group_attachment" "ireland" {
+resource "aws_lb_target_group_attachment" "test" {
   count            = length(aws_instance.test)
   target_group_arn = aws_lb_target_group.test.arn
   target_id        = aws_instance.test[count.index].id
